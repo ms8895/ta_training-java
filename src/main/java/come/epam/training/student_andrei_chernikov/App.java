c1cbd54484e6e7e0fcc6eed68d510a28d514ca43
ms8895 <ach910140@gmail.com>
2023-09-22 12:08:19 +0600

    Initial commit

---
+++ src/main/java/come/epam/training/student_andrei_chernikov/App.java
@@ -0,0 +1,13 @@
+package come.epam.training.student_andrei_chernikov;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/come/epam/training/student_andrei_chernikov/AppTest.java
@@ -0,0 +1,20 @@
+package come.epam.training.student_andrei_chernikov;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
