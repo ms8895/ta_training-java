8a7af4819f0b588567a20cabfa8c099383e934d1
ms8895 <ach910140@gmail.com>
2023-11-08 09:31:17 +0600

    Задание 1 "Meet AutoCode"
    Перейдите в класс HelloAutocode, и напишите простую программу, которая печатает: "Hello, Autocode!" (не печатайте кавычки).

---
+++ src/main/java/come/epam/training/student_andrei_chernikov/meetautocode/HelloAutocode.java
@@ -0,0 +1,12 @@
+package come.epam.training.student_andrei_chernikov.meetautocode;
+/*Цель задания — познакомиться с платформой AutoCode.
+
+Перейдите в класс HelloAutocode, и напишите простую программу, которая печатает: "Hello, Autocode!" (не печатайте кавычки).
+
+ */
+public class HelloAutocode {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, Autocode!");
+    }
+}
